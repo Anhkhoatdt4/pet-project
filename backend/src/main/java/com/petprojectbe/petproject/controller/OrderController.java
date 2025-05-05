a2a7c15097bb188cd7e61fda5e9f095053a53541
Anh Khoa <=khoanguyenanh218@gmail.com>
2025-05-05 16:44:17 +0700

    feat(backend): Implement order and payment services
    - Added OrderResponse DTO for order response handling
    - Implemented StripeService and PaymentIntentService for payment processing
    - Updated OrderService to support order creation with payment intent
    - Modified OAuth2Controller and OrderController for new flow
    - Updated application.yaml for configuration changes

--- backend/src/main/java/com/petprojectbe/petproject/controller/OrderController.java
+++ backend/src/main/java/com/petprojectbe/petproject/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.petprojectbe.petproject.controller;
 
 import com.petprojectbe.petproject.dto.request.OrderRequest;
+import com.petprojectbe.petproject.dto.response.OrderResponse;
 import com.petprojectbe.petproject.entity.Order;
 import com.petprojectbe.petproject.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/order")
@@ -19,8 +21,14 @@ public class OrderController {
     OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<?> createOrder(@RequestBody OrderRequest orderRequest, Principal principal) throws Exception {
-        Order order = orderService.createOrder(orderRequest,principal);
-        return new ResponseEntity<>(order, HttpStatus.OK);
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderRequest orderRequest, Principal principal) throws Exception {
+        OrderResponse orderResponse = orderService.createOrder(orderRequest,principal);
+        return new ResponseEntity<>(orderResponse,HttpStatus.OK);
+    }
+
+    @PostMapping("/update-payment")
+    public ResponseEntity<?> updatePaymentStatus(@RequestBody Map<String,String> request) throws Exception {
+        Map<String,String> response = orderService.updateStatus(request.get("paymentIntent"),request.get("status"));
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 }
