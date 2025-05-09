7d51e588d7cd19192b7fb6752c4fc9a672b423bf
Anh Khoa <=khoanguyenanh218@gmail.com>
2025-05-09 21:58:35 +0700

    Implement admin category and product management features

--- backend/src/main/java/com/petprojectbe/petproject/dto/request/CategoryDto.java
+++ backend/src/main/java/com/petprojectbe/petproject/dto/request/CategoryDto.java
@@ -14,5 +14,5 @@ public class CategoryDto {
     private String name;
     private String description;
     private String code;
-    private List<CategoryTypeDto> categoryTypeDtoList;
+    private List<CategoryTypeDto> categoryTypes;
 }
